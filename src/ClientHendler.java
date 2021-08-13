4c7915c575b04986b4a6e6f3a3aaf2c2aba987a7
Egor <ilinincorp@mail.ru>
2021-08-13 19:11:31 +0300

    Done

--- src/ClientHendler.java
+++ src/ClientHendler.java
@@ -4,6 +4,7 @@ import java.io.DataOutputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
+
 public class ClientHendler {
     Server server;
     Socket socket;
@@ -12,67 +13,77 @@ public class ClientHendler {
     private boolean authenticated;
     private String nickname;
 
-    public ClientHandler(Server server, Socket socket){
-        try{
+    public ClientHandler(Server server, Socket socket) {
+        try {
             this.server = server;
             this.socket = socket;
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
-            new Thread(() ->{
-                try{
+            new Thread(() -> {
+
+
+                try {
                     while (true) {
+
+
                         String str = in.readUTF();
-                        if (str.equals("/end")){
+                        if (str.equals("/end")) {
                             sendMsg("/end");
                             System.out.println("OffLine");
                             break;
                         }
-                    if(str.startsWith("/auth")){
-                        String[] token = str.split("\\s+");
-                        nickname = server.getAuthService()
-                                .getNicknameByLoginAndPassword(token[1],token[2]);
-                        if(nikname != null){
-                            server.subscribe(this);
-                            authenticated = true;
-                            sendMsg("/authok "+ nikname);
-                            break;
-                        } else{
-                            sendMsg("incorrecktLogin");
+
+
+                        if (str.startsWith("/auth")) {
+                            String[] token = str.split("\\s+");
+                            nickname = server.getAuthService()
+                                    .getNicknameByLoginAndPassword(token[1], token[2]);
+                            if (nikname != null) {
+                                server.subscribe(this);
+                                authenticated = true;
+                                sendMsg("/authok " + nikname);
+                                break;
+                            } else {
+                                sendMsg("incorrecktLogin");
+                            }
                         }
                     }
+
+
+                    while (authenticated) {
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            sendMsg("/end");
+                            System.out.println("OffLine");
+                            break;
+                        }
+                        server.broadcastMsg(this, str);
                     }
-                while(authenticated) {
-                    String str = in.readUTF();
-                    if (str.equals("/end")) {
-                        sendMsg("/end");
-                        System.out.println("OffLine");
-                        break;
-                    }
-                    server.broadcastMsg(this, str);
-                }
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
-                }finally{
+                } finally {
                     server.unsubscribe(this);
                     try {
                         socket.close();
-                    }catch (IOException e){
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             }).start();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void sendMsg(String msg){
-        try{
+
+    public void sendMsg(String msg) {
+        try {
             out.writeUTF(msg);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-public String getNickname(){
+
+    public String getNickname() {
         return nickname;
-}
+    }
 }
